a4d6458a8bc98ee5c6d016a98a6432d2bf2c301f
oliverlangmo <oliver.langmo@gmail.com>
2016-08-26 23:55:17 -0500

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,23 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+  def new
+    @todo = Todo.new
+  end
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:item)end
+end
